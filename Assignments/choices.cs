c1133a38e8a71ce02283765ce8a78fba694cd702
saswatmohanty95 <55775724+saswatmohanty95@users.noreply.github.com>
2019-10-18 02:37:14 +0530

    C# Assignment1&2

---
+++ Assignments/choices.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    class choices
+    {
+        double sum;
+        public choices()
+        {
+
+        }
+        public void total1()
+        {
+            int b;
+            Console.WriteLine("Total Bill: " + sum);
+            Console.WriteLine("\nDo you want to select more?\n1-Yes\n2-No");
+            b = int.Parse(Console.ReadLine());
+            if (b == 1)
+            {
+                choice();
+            }
+            else
+            {
+                Console.WriteLine("Your final bill amount is: " + sum);
+                Console.WriteLine("Have a Good Day!!");
+            }
+
+        }
+        public void choice()
+        {
+            int i;
+            Console.WriteLine("Select your Product:\n1-Redmi K20 Pro:Rs.29000\n2-Oneplus 7T:Rs.37000\n3-RealMe X2 Pro:Rs.28900\n4-Galaxy A70:Rs.26700\n5-Vivo Nex 3:Rs.41000\n");
+
+            i = int.Parse(Console.ReadLine());
+            switch (i)
+            {
+                case 1:
+                    int price = 29000;
+                    int qty;
+                    double total;
+                    Console.WriteLine("You have selected Redmi K20 Pro\n");
+                    Console.WriteLine("Enter the quantity\n");
+                    qty = int.Parse(Console.ReadLine());
+                    total = qty * price;
+                    sum = sum + total;
+                    total1();
+                    break;
+                case 2:
+                    price = 37000;
+                    Console.WriteLine("You have selected One Plus 7T\n");
+                    Console.WriteLine("Enter the quantity\n");
+                    qty = int.Parse(Console.ReadLine());
+                    total = qty * price;
+                    sum = sum + total;
+                    total1();
+                    break;
+                case 3:
+                    price = 28900;
+                    Console.WriteLine("You have selected RealMe X2 Pro\n");
+                    Console.WriteLine("Enter the quantity\n");
+                    qty = int.Parse(Console.ReadLine());
+                    total = qty * price;
+                    sum = sum + total;
+                    total1();
+                    break;
+                case 4:
+                    price = 26700;
+                    Console.WriteLine("You have selected Galaxy A70\n");
+                    Console.WriteLine("Enter the quantity\n");
+                    qty = int.Parse(Console.ReadLine());
+                    total = qty * price;
+                    sum = sum + total;
+                    total1();
+                    break;
+                case 5:
+                    price = 41000;
+                    Console.WriteLine("You have selected Vivo Nex 3\n");
+                    Console.WriteLine("Enter the quantity\n");
+                    qty = int.Parse(Console.ReadLine());
+                    total = qty * price;
+                    sum = sum + total;
+                    total1();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            choices ch = new choices();
+            ch.choice();
+
+
+
+
+
+        }
+    }
+}
---
+++ Assignments/shapes.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    class shapes
+    {
+        public shapes()
+        {
+
+        }
+        public shapes(int side)
+        {
+            int area;
+            area = side * side;
+            Console.WriteLine("Area of the given Square is:" + "" + area);
+        }
+        public shapes(int l, int b)
+        {
+            int area = l * b;
+            Console.WriteLine("Area of the given Rectangle is: " + "" + area);
+        }
+        public void square()
+        {
+
+
+        }
+        public void rect()
+        {
+
+
+        }
+        public void cir()
+        {
+            int r;
+            double area;
+            Console.WriteLine("Enter the radius of circle");
+            r = int.Parse(Console.ReadLine());
+            area = (3.14) * r * r;
+            Console.WriteLine("Area of the given Circle is: " + "" + area);
+        }
+        public void tri()
+        {
+            int b, h;
+            double area;
+            Console.WriteLine("Enter height of triangle");
+            h = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter base of traingle");
+            b = int.Parse(Console.ReadLine());
+            area = 0.5 * b * h;
+            Console.WriteLine("Area of the given Triangle is: " + "" + area);
+        }
+
+        static void Main(string[] args)
+        {
+            shapes s = new shapes();
+            int shape;
+            Console.WriteLine("Please Choose required Shape:\n 1-Square\n 2-Rectangle\n 3-Circle\n 4-Triangle");
+            shape = int.Parse(Console.ReadLine());
+
+            switch (shape)
+            {
+                case 1:
+                    Console.WriteLine("You have selected Square\n");
+                    Console.WriteLine("Enter the side of the square");
+                    int side;
+                    side = int.Parse(Console.ReadLine());
+                    shapes sqa = new shapes(side);
+                    break;
+                case 2:
+                    Console.WriteLine("You have selected Rectangle\n");
+                    int l, b;
+                    Console.WriteLine("Enter the length of Rectangle");
+                    l = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the breadth of Rectangle");
+                    b = int.Parse(Console.ReadLine());
+
+                    shapes rec = new shapes(l, b);
+
+
+                    break;
+                case 3:
+                    Console.WriteLine("You have selected Circle\n");
+                    s.cir();
+                    break;
+                case 4:
+                    Console.WriteLine("You have selected Triangle\n");
+                    s.tri();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
+    }
+}
+
+
