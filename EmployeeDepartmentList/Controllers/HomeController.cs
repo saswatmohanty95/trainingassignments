d282181e511fefa444a98b17359181d26359862d
saswatmohanty95 <55775724+saswatmohanty95@users.noreply.github.com>
2019-11-13 16:01:51 +0530

    MVC Assessment

---
+++ EmployeeDepartmentList/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeDepartmentList.Models;
+
+namespace EmployeeDepartmentList.Controllers
+{
+    public class HomeController : Controller
+    {
+        employeedeptdataEntities entities = new employeedeptdataEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+          
+            return View(entities.Employees.ToList());
+
+        }
+
+        public ActionResult Index1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index1(int id)
+        {
+            TempData["a"] = id;
+            return RedirectToAction("Display");
+        }
+        public ActionResult Display()
+        {
+            int id = Convert.ToInt32(TempData["a"]);
+            List<Employee> sid = entities.Employees.Where(x => x.Departmentid == id).ToList();
+            return View(sid);
+        }
+    }
+}
